b1f8c675c55cc7d90ce0b3cdd4aac471c8619c55
Mohamed Ahmed Abdel Fattah <mohamedahmedabdelfattah@gmail.com>
2015-02-24 17:37:18 +0200

    Initial Commit

---
+++ ImageViewerLib/Image.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using openCV;
+using System.Drawing;
+
+namespace ImageViewerLib
+{
+    public class Image
+    {
+
+        public IplImage ResizeImage(Image originalImage, Int32 newWidth, Int32 newHeight)
+        {
+            IplImage img, resizedimage;
+
+            //cvlib.CvCreateImage();
+
+            Int32 width = newWidth, height = newHeight;
+
+            CvSize size = new CvSize(width, height);
+
+            resizedimage = cvlib.CvCreateImage(size, img.depth, img.nChannels);
+
+            cvlib.CvResize(ref originalImage, ref resizedimage, cvlib.CV_INTER_LINEAR);
+
+            return resizedimage;
+        }
+
+    }
+}
